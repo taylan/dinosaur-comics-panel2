8e7a276784cd723b4f401ed889f8c7186544c56a
Taylan Aydinli <taylanaydinli@gmail.com>
2014-02-05 11:51:30 +0200

    Fixed comic retrieval method
    Turns out the comic id and the file name of the comic
    don't always match. So the app now retrieves the html
    of the comic and gets the actual file name of that comic
    and downloads that png file instead of going
    /comic2-{comic_id}.png

--- dc-panel2.py
+++ dc-panel2.py
@@ -8,15 +8,15 @@ from wand.image import Image
 
 app = Flask(__name__)
 hdrs = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.102 Safari/537.36'}
-comic_id_re = compile(r'comic2-(\d+)\.png$')
+comic_id_re = compile(r'comic=(\d+)$')
 
 
 def get_max_comic_id():
-    return comic_id_re.search(BS(get('http://www.qwantz.com/index.php', headers=hdrs).text).select('.comic')[0]['src']).group(1)
+    return comic_id_re.search(BS(get('http://www.qwantz.com/index.php', headers=hdrs).text).find('meta', attrs={'property': 'og:url'})['content']).group(1)
 
 
 def save_panel2(comic_id):
-    r = get('http://www.qwantz.com/comics/comic2-{0}.png'.format(comic_id), stream=True)
+    r = get(BS(get('http://www.qwantz.com/index.php?comic={0}'.format(comic_id), headers=hdrs).text).select('.comic')[0]['src'], stream=True)
     fn = path.join(path.dirname(__file__), 'static/comics/{0}.png'.format(comic_id))
     if r.status_code == 200:
         with open(fn, 'wb') as f:
