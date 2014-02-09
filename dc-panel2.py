0d225eef7515ec4fafd6abcc22be1f9420623f30
Taylan Aydinli <taylanaydinli@gmail.com>
2014-02-09 15:35:21 +0200

    Made file resize operation more robust
    File resize is tried until it succeeds.

--- dc-panel2.py
+++ dc-panel2.py
@@ -15,6 +15,16 @@ def get_max_comic_id():
     return comic_id_re.search(BS(get('http://www.qwantz.com/index.php', headers=hdrs).text).find('meta', attrs={'property': 'og:url'})['content']).group(1)
 
 
+def _resize_image(filename):
+    try:
+        with Image(filename=filename) as img:
+            img.crop(243, 0, width=131, height=244)
+            img.save(filename=filename)
+            return True
+    except:
+        return False
+
+
 def save_panel2(comic_id):
     r = get(BS(get('http://www.qwantz.com/index.php?comic={0}'.format(comic_id), headers=hdrs).text).select('.comic')[0]['src'], stream=True)
     fn = path.join(path.dirname(__file__), 'static/comics/{0}.png'.format(comic_id))
@@ -23,9 +33,10 @@ def save_panel2(comic_id):
             for chunk in r.iter_content(1024):
                 f.write(chunk)
 
-    with Image(filename=fn) as img:
-        img.crop(243, 0, width=131, height=244)
-        img.save(filename=fn)
+    while True:
+        result = _resize_image(fn)
+        if result:
+            break
 
 
 @app.route('/', methods=['GET'])
