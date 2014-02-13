3dc789878dd67a230957db454531c8f266b620d0
Taylan Aydinli <taylanaydinli@gmail.com>
2014-02-13 13:44:48 +0200

    Added progress bar to scraper

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from sys import argv
 from os import path
 from dinosaurcomics import get_max_comic_id, panels
 from dinosaurcomics.DCComic import DCComic
+from pyprind import ProgPercent
 
 
 def main():
@@ -10,13 +11,16 @@ def main():
     except IndexError:
         comics_dir = path.join(path.dirname(__file__), 'static/comics')
 
-    for comic_id in range(1, get_max_comic_id()+1):
+    max_comic_id = get_max_comic_id()
+    print('Scraping {0} comics...'.format(max_comic_id))
+    progbar = ProgPercent(max_comic_id, stream=1)
+    for comic_id in range(1, max_comic_id+1):
         comic = DCComic(comic_id, comics_dir)
         comic.save_comic()
         for panel in panels.keys():
             comic.save_panel(panel)
 
-        print('Comic {0} complete.'.format(comic_id))
+        progbar.update()
 
 
 if __name__ == '__main__':
