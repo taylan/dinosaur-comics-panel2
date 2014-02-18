67b032c6efaa343da1a7a9d0ae22e49ea6d8ab0a
Taylan Aydinli <taylanaydinli@gmail.com>
2014-02-18 10:16:50 +0200

    Refactored code. Added AJAX handler for random panel

--- dc-panel2.py
+++ dc-panel2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from os import path
 from dinosaurcomics import get_random_comic_id
 from dinosaurcomics.DCComic import DCComic
@@ -13,11 +13,27 @@ def save_panel(comic_id, panel):
     comic.save_panel(panel)
 
 
-@app.route('/random-panel/<int:panel>', methods=['GET'])
-def random_panel(panel=2):
+def _do_random_panel(panel):
     comic_id = get_random_comic_id()
     save_panel(comic_id, panel)
+    return comic_id
+
+
+@app.route('/random-panel/<int:panel>', methods=['GET'])
+def random_panel(panel=2):
+    comic_id = _do_random_panel(panel)
     return render_template('index.html', comic_id=comic_id, panel=panel)
+
+
+@app.route('/a/random-panel/<int:panel>', methods=['GET'])
+def ajax_random_panel(panel=2):
+    comic_id = _do_random_panel(panel)
+    return jsonify({'comic': comic_id, 'panel': panel,
+                    'panel_url': '/static/comics/{0}_{1}.png'.format(comic_id,
+                                                                     panel),
+                    'comic_url': 'http://www.qwantz.com/index.php?comic={0}'.format(
+                        comic_id)}
+    )
 
 
 @app.route('/', methods=['GET'])
